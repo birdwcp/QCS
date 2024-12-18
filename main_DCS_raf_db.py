bf841dfcb4b0dc6a5067582e4a6294df352d3bfe
birdwcp <wcp12.4@qq.com>
2024-12-18 15:02:15 +0800

    Update main_DCS_raf_db.py

--- main_DCS_raf_db.py
+++ main_DCS_raf_db.py
@@ -113,6 +113,7 @@ def main():
     data_transforms = {
         'train': transforms.Compose([transforms.Resize((224, 224)),
             transforms.RandomHorizontalFlip(),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             transforms.RandomErasing(scale=(0.02, 0.1))]),
